a68e4608ce3996389b7973a36adabc32bc44d171
nguyenduyhung2908 <nguyenduyhung2003xx@gmail.com>
2023-04-08 22:09:43 +0700

    upload code

--- src/main/java/org/duyhung/assignment/service/INhaSanXuatService.java
+++
@@ -1,9 +0,0 @@
-package org.duyhung.assignment.service;
-
-import org.duyhung.assignment.entity.ChucVu;
-import org.duyhung.assignment.entity.NhaSanXuat;
-
-import java.util.List;
-
-public interface INhaSanXuatService extends CRUDService<NhaSanXuat,String> {
-}
